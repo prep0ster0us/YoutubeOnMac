0659b3c2a9a1c33d369ea28f1137be327f023c5d
prep0ster0us <rbabu1@unh.newhaven.edu>
2025-04-13 19:51:51 -0400

    Initial Commit

---
+++ YoutubeOnMac/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  YoutubeOnMac
+//
+//  Created by Ritwik Babu on 4/13/25.
+//
+
+import SwiftUI
+import WebKit
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YoutubeOnMac/YoutubeOnMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeOnMacApp.swift
+//  YoutubeOnMac
+//
+//  Created by Ritwik Babu on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct YoutubeOnMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
